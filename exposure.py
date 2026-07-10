15c7eecf3bd3a73dea6b72b97a097e8e34c58a80
Dr. Shemin T John <sheminjohn123@gmail.com>
2026-07-10 12:34:35 +0530

    Added exposure module

---
+++ exposure.py
@@ -0,0 +1,7 @@
+def calculate_exposure(asset_value):
+    """Return insured asset value."""
+    return asset_value
+
+
+if __name__ == "__main__":
+    print("Exposure =", calculate_exposure(1_000_000))
